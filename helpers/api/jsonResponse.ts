819ffc5eeb4399e007a6661895ca03cd7b4818b2
terthesz <ludma.matej@gmail.com>
2023-08-16 13:44:17 +0200

    🚀 repo change-ä

--- helpers/api/jsonResponse.ts
+++ helpers/api/jsonResponse.ts
@@ -5,23 +5,6 @@ export default function jsonResponse(json: any, status?: number) {
     status: status || json.status || 200,
     headers: {
       'content-type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     },
   });
-}
-
-export function corsResponse() {
-  return NextResponse.json(
-    {},
-    {
-      headers: {
-        'content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    }
-  );
 }
